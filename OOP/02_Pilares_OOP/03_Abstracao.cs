43891bd2600858b175772d06704b403170a4f5a7
George Duarte <contato@georgeduarte.com.br>
2022-06-30 20:51:05 -0300

    Abstração

---
+++ OOP/02_Pilares_OOP/03_Abstracao.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP
+{
+    // Abstrair uma especialização - Conceito de super classe sem forçar implementação
+    // Uma classe abestrata só pode ser herdada e nunca instanciada
+    public abstract class Eletrododomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected Eletrododomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+
+        // Como Abstrato, não fica obrigatório implementar
+        public abstract void Ligar();
+        public abstract void Desligar();
+    }
+}
