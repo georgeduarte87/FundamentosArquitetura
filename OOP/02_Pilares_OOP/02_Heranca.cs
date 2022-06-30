6abcd677c1a3f8ee81a23d63da2e1d19f971e87d
George Duarte <contato@georgeduarte.com.br>
2022-06-30 20:43:04 -0300

    Herança

---
+++ OOP/02_Pilares_OOP/02_Heranca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP
+{
+    // Herança - Ocorre sempre que algo é outra coisa - Um funcionário é uma pessoa - Herda do mais universal no contexto
+    public class Funcionario : Pessoa 
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            // Quando há herança, as atribuições ocorrem de forma natural
+            var funcionario = new Funcionario()
+            {
+                Nome = "João das Couve",
+                DataNascimento = Convert.ToDateTime("1990/01/01"),
+                DataAdmissao = DateTime.Now,
+                Registro = "0123456"
+            };
+        }
+    }
+}
