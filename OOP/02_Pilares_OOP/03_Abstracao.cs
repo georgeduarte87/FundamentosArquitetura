164a865187732ee6f225beba273f2fbb01af11a8
George Duarte <contato@georgeduarte.com.br>
2022-06-30 21:06:17 -0300

    Polimorfismo

--- OOP/02_Pilares_OOP/03_Abstracao.cs
+++ OOP/02_Pilares_OOP/03_Abstracao.cs
@@ -14,8 +14,20 @@
         }
 
 
-        // Como Abstrato, não fica obrigatório implementar
+        // Como Abstrato, Não permite implementar, mas obriga a sobrescrever
         public abstract void Ligar();
         public abstract void Desligar();
+
+        // Sem Abstract - Te obriga a implementar, mas não permite sobrescrever
+        public void Testar()
+        {
+            // Fazer teste do equipamento
+        }
+
+        // Ser abstract, mas com Virtual - Não te obriga a sobrescrever, mas precisa implementar
+        public virtual void TestarNoamente()
+        {
+            // Fazer teste do equipamento
+        }
     }
 }
---
+++ OOP/02_Pilares_OOP/04_Polimorfismo.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP
+{
+    public class CafeteiraEspressa : Eletrododomestico
+    {
+        public CafeteiraEspressa(string nome, int voltagem) : base(nome, voltagem) { }
+
+        public CafeteiraEspressa() : base("Padrão", 220) { }
+
+        private static void AquecerAgua() { }
+
+        private static void MoerGraos() { }
+
+        public void PrepagarCafe()
+        {
+            Testar();
+            AquecerAgua();
+            MoerGraos();
+            TestarNoamente();
+            // ETC
+        }
+
+        // Não é possível sobrescrever, pois não é abstract e nem virtual
+        //public override Testar() { }
+
+        // Posso sobrescrever por ser virtual
+        public override void TestarNoamente()
+        {
+            // Teste de café
+        }
+
+        // Sou obrigado a sobrescrever por ser abstract
+        public override void Ligar()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+
+        // Sou obrigado a sobrescrever por ser abstract
+        public override void Desligar()
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}
